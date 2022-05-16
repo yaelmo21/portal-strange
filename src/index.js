f62617e711b5caedcf76852443ef2c4b4b62cb72
Yael Ruiz Pucheta <yaelruiz99@outlook.com>
2022-05-16 15:05:51 -0500

    Reset button

--- src/index.js
+++ src/index.js
@@ -9,31 +9,41 @@ const buttonUpload = document.getElementById('button-upload');
 const downloadImage = document.getElementById('download-image');
 const containerPortal = document.getElementById('container-portal');
 const footer = document.querySelector('footer');
+const buttonReset = document.getElementById('button-reset');
+
+
+buttonReset.style.visibility = 'hidden';
 downloadImage.style.visibility = 'hidden';
 containerPortal.style.display = 'none';
 
 const renderPortal = () => {
-    for (let i = 0; i < 360; i++) {
-        const chispa = document.createElement('div');
-        chispa.className = 'chispa';
-        const chispaTranslate = Math.random() * (250 - 220) + 220;
-        chispa.style.transform = `rotate(${i * 2}deg) translate(${chispaTranslate}px)`;
-        portal.appendChild(chispa);
+    const countChildren = portal.childNodes.length;
+    if (countChildren === 0) {
+        for (let i = 0; i < 360; i++) {
+            const chispa = document.createElement('div');
+            chispa.className = 'chispa';
+            const chispaTranslate = Math.random() * (250 - 220) + 220;
+            chispa.style.transform = `rotate(${i * 2}deg) translate(${chispaTranslate}px)`;
+            portal.appendChild(chispa);
+        }
     }
 }
 
 buttonUpload.onclick = () => inputFile.click();
 
-inputFile.onchange = (event) => {
-    const file = event.target.files[0];
+inputFile.onchange = ({ target }) => {
+    const file = target.files[0];
+    if (!file) return;
     const reader = new FileReader();
     reader.readAsDataURL(file);
     reader.onload = () => {
         image.src = reader.result;
         containerPortal.style.display = 'block';
         downloadImage.style.visibility = 'visible';
+        buttonReset.style.visibility = 'visible';
         renderPortal();
     }
+    inputFile.value = '';
 }
 
 
@@ -48,4 +58,11 @@ downloadImage.onclick = async() => {
     link.download = 'portal-strange.png';
     link.href = imageData;
     link.click();
+}
+
+buttonReset.onclick = () => {
+    containerPortal.style.display = 'none';
+    downloadImage.style.visibility = 'hidden';
+    buttonReset.style.visibility = 'hidden';
+    image.src = '';
 }
